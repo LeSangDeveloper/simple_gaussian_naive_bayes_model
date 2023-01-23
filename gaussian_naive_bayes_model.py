4d98a641ecd38de15c7c630728c296695d2085ec
Lê Sang <42503707+LeSangDeveloper@users.noreply.github.com>
2023-01-23 17:29:56 +0800

    finish Gaussian NB

--- gaussian_naive_bayes_model.py
+++ gaussian_naive_bayes_model.py
@@ -1,9 +1,70 @@
+import numpy as np
+from math import sqrt
+from math import exp
+from math import pi
+
 class GaussianNB():
     def __init__(self):
         print("Call init")
     
     def __call__(self, X):
         print("call __call__()")
+        results = []
+        for x in X:
+            result = dict()
+            for class_name, _ in self.cls_number.items():
+                i = 0
+                result_value = self.prior[class_name]
+                for xi in x:
+                    result_value *= self.calculate_probability(xi, self.mean[class_name][i], self.var[class_name][i])
+                    i += 1
+                result[class_name] = result_value
+            
+            total = 0
+            for _, value in result.items():
+                total += value
+            for key, value in result.items():
+                result[key] = value / total
+
+            results.append(result)
+        return results
 
     def fit(self, X, y):
         print("call fit()")
+        # identify classes
+        classes = np.unique(y)
+        self.cls_number = dict()
+        i = 0
+        for class_name in classes:
+            self.cls_number[class_name] = i
+            i += 1
+
+        # run loop for each class
+        self.doc_in_class = dict()
+        self.prior = dict()
+        self.mean = dict()
+        self.var = dict()
+
+        for class_name, _ in self.cls_number.items():
+            self.doc_in_class[class_name] = np.count_nonzero(y == class_name)
+            self.prior[class_name] = self.doc_in_class[class_name] / len(y)
+            X_c = []
+            for x, label in zip(X, y):
+                if label == class_name:
+                    X_c.append(x)
+            self.mean[class_name] = self.mean_fn(X_c)
+            self.var[class_name] = self.stdev(X_c)
+
+        return self
+
+    def calculate_probability(self, x, mean, stdev):
+        exponent = exp(-((x-mean)**2 / (2 * stdev**2 )))
+        return (1 / (sqrt(2 * pi) * stdev)) * exponent
+
+    def mean_fn(self, X):
+        return np.sum(X, axis=0)/float(len(X))
+
+    def stdev(self, X):
+        avg = self.mean_fn(X)
+        variance = sum([(x - avg)**2 for x in X]) / float(len(X) - 1)
+        return np.sqrt(variance)
--- test.py
+++ test.py
@@ -1,5 +1,22 @@
 from gaussian_naive_bayes_model import GaussianNB
+import numpy as np
+
+# Test summarizing by class
+dataset = [[3.393533211,2.331273381],
+ [3.110073483,1.781539638],
+ [1.343808831,3.368360954],
+ [3.582294042,4.67917911],
+ [2.280362439,2.866990263],
+ [7.423436942,4.696522875],
+ [5.745051997,3.533989803],
+ [9.172168622,2.511101045],
+ [7.792783481,3.424088941],
+ [7.939820817,0.791637231]]
+labels = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+train_data = np.array(dataset)
+label = np.array(labels)
 
 model = GaussianNB()
-model.fit([2, 2], [1, 1])
-model([1, 2, 3])
+model.fit(dataset, label)
+print(model(np.array([[3.393533211,2.331273381], [7.939820817,0.791637231]])))
